9b9c04400ed8f26cd77b7d79d90b2096258ffac4
Kami <51175640+hfurusho@users.noreply.github.com>
2019-10-21 13:27:37 -0700

    Current weather mostly populates after api call.

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,37 @@
-.current-weather-conditions {
-  list-style-type: none;
+const APIKEY = "79564d65443999fbd44fa53717a9b123";
 
+$("#citySearchBtn").on("click", function(event) {
+  event.preventDefault();
+  getWeatherConditions();
+});
+
+function getWeatherConditions() {
+  // let citySearched = $("citySearch").val(); // TODO: Uncomment for live version.
+  let citySearched = "seattle"; // For testing
+  let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${citySearched}&units=imperial&appid=${APIKEY}`;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    populateCurrentWeatherConditions(response);
+  });
+}
+
+function populateCurrentWeatherConditions(results) {
+  let cityName = results.name;
+  let temperature = results.main.temp;
+  let humidity = results.main.humidity;
+  let windSpeed = results.wind.speed;
+  let currentWeather = results.weather.main;
+  let iconURL = `http://openweathermap.org/img/w/${results.weather[0].icon}.png`;
+
+  $("#currentCity").text(cityName);
+  $("#todaysDate").text("(10/21/29)"); // TODO: Get current date and format.
+  $("#currentWeatherIcon").attr("src", iconURL);
+  $("#currentWeatherIcon").attr("alt", currentWeather + " icon");
+  $("#todaysTemp").text(temperature);
+  $("#todaysHumidity").text(humidity);
+  $("#todaysWindSpeed").text(windSpeed);
+  $("#todaysUvIndex").text();
 }
