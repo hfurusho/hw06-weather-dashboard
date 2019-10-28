0d0ed4651b8ebff3c3de9dc4f876fdce84bde680
Kami <51175640+hfurusho@users.noreply.github.com>
2019-10-27 19:27:33 -0700

    Forecast and current weather store in localStorage.

--- scripts/app.js
+++ scripts/app.js
@@ -8,42 +8,108 @@ $("#citySearchBtn").on("click", function(event) {
   getFiveDayForecast(citySearched);
 });
 
+function populateSearchHistory() {
+  $("#searchHistoryList").empty();
+  let searchHistory = JSON.parse(localStorage.getItem("storedWeatherData"))
+    .searchHistory;
+  for (let i = 0; i < searchHistory.length; i++) {
+    let item = $("<li class='list-group-item'></li>");
+    item.text(searchHistory[i].cityName);
+    $("#searchHistoryList").prepend(item);
+  }
+  $(".list-group-item").on("click", function() {
+    getCurrentWeatherConditions($(this).text());
+    getFiveDayForecast($(this).text());
+  });
+}
+
+function getStoredWeatherData() {
+  let storedWeatherData = JSON.parse(localStorage.getItem("storedWeatherData"));
+  if (!storedWeatherData) {
+    return {
+      searchHistory: [],
+      data: {
+        currentWeather: [],
+        forecast: []
+      }
+    };
+  } else {
+    return storedWeatherData;
+  }
+}
+
 function getCurrentWeatherConditions(citySearched) {
   let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${citySearched}&units=imperial&appid=${APIKEY}`;
-
+  let storedWeatherData = getStoredWeatherData();
+  let searchHistory = storedWeatherData.searchHistory;
+  let timeNow = new Date().getTime();
+  citySearched = citySearched.toLowerCase().trim();
+  for (let i = 0; i < searchHistory.length; i++) {
+    if (
+      searchHistory[i].cityName.toLowerCase() == citySearched &&
+      timeNow < searchHistory[i].dt * 1000 + 600000
+    ) {
+      for (let j = 0; j < storedWeatherData.data.currentWeather.length; j++) {
+        if (
+          storedWeatherData.data.currentWeather[j].name.toLowerCase() ==
+          citySearched
+        ) {
+          console.log("from storage");
+          populateCurrentWeatherConditions(
+            storedWeatherData.data.currentWeather[j]
+          );
+          return;
+        }
+      }
+    }
+  }
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(results) {
+    console.log("looked up");
     populateCurrentWeatherConditions(results);
+    storeCurrentWeather(results);
   });
+}
+
+function storeCurrentWeather(results) {
+  let storedWeatherData = getStoredWeatherData();
+  let searchHistoryEntry = {
+    cityName: results.name,
+    dt: results.dt
+  };
+  storedWeatherData.searchHistory.push(searchHistoryEntry);
+  storedWeatherData.data.currentWeather.push(results);
+  localStorage.setItem("storedWeatherData", JSON.stringify(storedWeatherData));
 }
 
 function populateCurrentWeatherConditions(results) {
   let cityName = results.name;
-  let today = new Date();
-  let temp = results.main.temp;
+  let date = new Date(results.dt * 1000);
+  let description = results.weather[0].main;
   let humidity = results.main.humidity;
-  let windSpeed = results.wind.speed;
-  let currentWeather = results.weather[0].main;
   let iconURL = `http://openweathermap.org/img/w/${results.weather[0].icon}.png`;
+  let temp = results.main.temp;
+  let windSpeed = results.wind.speed;
+
   let lon = results.coord.lon;
   let lat = results.coord.lat;
 
   $("#currentCity").text(cityName);
   $("#todaysDate").text(
-    `(${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()})`
-  ); // USE result.dt time (unix) and convert
+    `(${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()})`
+  );
   $("#currentWeatherIcon").attr("src", iconURL);
-  $("#currentWeatherIcon").attr("alt", currentWeather + " icon");
+  $("#currentWeatherIcon").attr("alt", description + " icon");
   $("#todaysTemp").text(temp);
   $("#todaysHumidity").text(humidity);
   $("#todaysWindSpeed").text(windSpeed);
 
-  populateCurrentUVIndex(lon, lat);
+  populateUVIndex(lon, lat);
 }
 
-function populateCurrentUVIndex(lon, lat) {
+function populateUVIndex(lon, lat) {
   let UVIndexURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${APIKEY}&lat=${lat}&lon=${lon}`;
   $.ajax({
     url: UVIndexURL,
@@ -66,17 +132,50 @@ function populateCurrentUVIndex(lon, lat) {
 }
 
 function getFiveDayForecast(citySearched) {
+  let storedWeatherData = getStoredWeatherData();
   let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${citySearched}&units=imperial&appid=${APIKEY}`;
-
+  let today = new Date().getDate();
+  for (let i = 0; i < storedWeatherData.searchHistory.length; i++) {
+    let savedDate = new Date(
+      storedWeatherData.searchHistory[i].dt * 1000
+    ).getDate();
+    if (
+      storedWeatherData.searchHistory[i].cityName.toLowerCase() ==
+        citySearched &&
+      savedDate == today
+    ) {
+      for (let j = 0; j < storedWeatherData.data.forecast.length; j++) {
+        if (
+          storedWeatherData.data.forecast[j].city.name.toLowerCase() ==
+          citySearched.toLowerCase()
+        ) {
+          console.log("forecast from storage");
+          populateForecast(storedWeatherData.data.forecast[j]);
+          populateSearchHistory();
+          return;
+        }
+      }
+    }
+  }
   $.ajax({
     url: forecastURL,
     method: "GET"
   }).then(function(results) {
     populateForecast(results);
+    storeForecast(results, citySearched);
+    populateSearchHistory();
   });
 }
 
+function storeForecast(results, citySearched) {
+  citySearched = citySearched.toLowerCase().trim();
+  let storedWeatherData = getStoredWeatherData();
+  storedWeatherData.data.forecast.push(results);
+  localStorage.setItem("storedWeatherData", JSON.stringify(storedWeatherData));
+}
+
 function populateForecast(results) {
+  $("#forecast").empty();
   let list = results.list;
   let daysForecasted = 0; // Used to check that only 5 days are forecasted
   for (let i = 0; i < list.length && daysForecasted < 6; i++) {
@@ -102,7 +201,7 @@ function populateForecast(results) {
       cardBodyDiv.append(dateDiv, imgElem, pTemp, pHumid);
       cardDiv.append(cardBodyDiv);
       colDiv.append(cardDiv);
-      $(".weather-conditions").append(colDiv);
+      $("#forecast").append(colDiv);
 
       daysForecasted++;
     }
